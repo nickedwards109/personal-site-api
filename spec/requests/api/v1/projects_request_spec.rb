f4d63ec78618cdfce8f55643a02caf3106351dfd
nickedwards109 <nicholas.lee.edwards@gmail.com>
2019-02-17 16:50:05 -0500

    Specify projects response containing HTML links
    This commit specifies how the projects index endpoint should respond with data in order to enable the front-end app to distinguish HTML links from other text in a paragraph.
    
    A GET request to '/api/v1/projects.json' should result in a response containing an array of JSON objects that each represent a project. Each project should have:
      - a title
      - an index to indicate the order of the project on the page
      - an array of paragraphs
      - an array of images
    
    Each paragraph should contain:
      - the foreign key of the project it belongs to
      - an index to indicate the order of the paragraph in the project
      - an array of sentence groups
      - an array of links
    
    Each sentence group should contain:
      - the foreign key of the paragraph it belongs to
      - an index to indicate the order of the sentence group in the paragraph
      - copy text
    
    Each link should contain:
      - the foreign key of the paragraph it belongs to
      - an index to indicate the order of the link in the paragraph
      - a url

--- spec/requests/api/v1/projects_request_spec.rb
+++ spec/requests/api/v1/projects_request_spec.rb
@@ -1,18 +1,34 @@
 require 'rails_helper'
 
 describe "Projects API" do
-  it "sends a collection of projects" do
+  before(:all) do
     # Start the tests with a clean database
+    SentenceGroup.destroy_all
+    Link.destroy_all
+    Image.destroy_all
+    Paragraph.destroy_all
     Project.destroy_all
 
-    project_1 = Project.create(title: "This is the first Project title")
-    paragraphs = create_list(:paragraph, 2, project: project_1)
-    images = create_list(:image, 2, project: project_1)
+    @project_1 = create(:project)
+      @images_1 = create_list(:image, 2, project: @project_1)
+      @paragraph_1 = create(:paragraph, project: @project_1)
+        @sentence_groups_1 = create_list(:sentence_group, 2, paragraph: @paragraph_1)
+        @links_1 = create_list(:link, 2, paragraph: @paragraph_1)
+      @paragraph_1_another = create(:paragraph, project: @project_1)
+        @sentence_groups_1_another = create_list(:sentence_group, 2, paragraph: @paragraph_1_another)
+        @links_1_another = create_list(:link, 2, paragraph: @paragraph_1_another)
 
-    project_2 = Project.create(title: "This is another Project title")
-    paragraphs = create_list(:paragraph, 2, project: project_2)
-    images = create_list(:image, 2, project: project_2)
+    @project_2 = create(:project)
+      @images_2 = create_list(:image, 2, project: @project_2)
+      @paragraph_2 = create(:paragraph, project: @project_2)
+        @sentence_groups_2 = create_list(:sentence_group, 2, paragraph: @paragraph_2)
+        @links_2 = create_list(:link, 2, paragraph: @paragraph_2)
+      @paragraph_2_another = create(:paragraph, project: @project_2)
+        @sentence_groups_2_another = create_list(:sentence_group, 2, paragraph: @paragraph_2)
+        @links_1_another = create_list(:link, 2, paragraph: @paragraph_2)
+  end
 
+  it "sends a collection of projects" do
     get '/api/v1/projects.json'
 
     expect(response).to be_successful
@@ -25,18 +41,46 @@ describe "Projects API" do
     expect(project).to have_key("title")
     expect(project["title"].class).to eq(String)
 
-    expect(project).to have_key("paragraphs")
-    expect(project["paragraphs"].class).to eq(Array)
-    paragraph = project["paragraphs"].first
-    expect(paragraph["copy"].class).to eq(String)
-    expect(paragraph["order_index"].class).to eq(Integer)
-    expect(paragraph["project_id"].class).to eq(Integer)
-
     expect(project).to have_key("images")
+    expect(project["images"].count).to eq(2)
     expect(project["images"].class).to eq(Array)
     image = project["images"].first
+    expect(image).to have_key("url")
+    expect(image).to have_key("order_index")
+    expect(image).to have_key("project_id")
     expect(image["url"].class).to eq(String)
     expect(image["order_index"].class).to eq(Integer)
     expect(image["project_id"].class).to eq(Integer)
+
+    expect(project).to have_key("paragraphs")
+    expect(project["paragraphs"].count).to eq(2)
+    expect(project["paragraphs"].class).to eq(Array)
+    paragraph = project["paragraphs"].first
+    expect(paragraph).to have_key("order_index")
+    expect(paragraph).to have_key("project_id")
+    expect(paragraph["order_index"].class).to eq(Integer)
+    expect(paragraph["project_id"].class).to eq(Integer)
+
+    expect(paragraph).to have_key("sentence_groups")
+    expect(paragraph["sentence_groups"].class).to eq(Array)
+    expect(paragraph["sentence_groups"].count).to eq(2)
+    sentence_group = paragraph["sentence_groups"].first
+    expect(sentence_group).to have_key("copy")
+    expect(sentence_group).to have_key("order_index")
+    expect(sentence_group).to have_key("paragraph_id")
+    expect(sentence_group["copy"].class).to eq(String)
+    expect(sentence_group["order_index"].class).to eq(Integer)
+    expect(sentence_group["paragraph_id"].class).to eq(Integer)
+
+    expect(paragraph).to have_key("links")
+    expect(paragraph["links"].class).to eq(Array)
+    expect(paragraph["links"].count).to eq(2)
+    link = paragraph["links"].first
+    expect(link).to have_key("url")
+    expect(link).to have_key("order_index")
+    expect(link).to have_key("paragraph_id")
+    expect(link["url"].class).to eq(String)
+    expect(link["order_index"].class).to eq(Integer)
+    expect(link["paragraph_id"].class).to eq(Integer)
   end
 end
