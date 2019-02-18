d63daf4d1e5b0dfd2a0fbddc944818d5013bc659
nickedwards109 <nicholas.lee.edwards@gmail.com>
2019-02-18 11:56:18 -0500

    Implement projects response containing HTML links
    This commit implements the projects response with a data format that will enable the front-end app to distinguish HTML links from other text in a paragraph. This will facilitate making the HTML links clickable in the front-end app.
    
    A GET request to '/api/v1/projects.json' results in a response containing an array of JSON objects that each represent a project. Each project object contains:
      - a title
      - an index to indicate the order of the project on the page
      - an array of paragraphs
      - an array of images
    
    Each paragraph object contains:
      - the foreign key of the project it belongs to
      - an index to indicate the order of the paragraph in the project
      - an array of sentence groups
      - an array of links
    
    Each sentence group object contains:
      - the foreign key of the paragraph it belongs to
      - an index to indicate the order of the sentence group in the paragraph
      - copy text
    
    Each link object contains:
      - the foreign key of the paragraph it belongs to
      - an index to indicate the order of the link in the paragraph
      - a url

--- app/controllers/api/v1/projects/projects_controller.rb
+++ app/controllers/api/v1/projects/projects_controller.rb
@@ -4,8 +4,15 @@ class Api::V1::Projects::ProjectsController < ApplicationController
 
     serialized_projects = projects.map do |project|
       title = project.title
-      paragraphs = project.paragraphs
       images = project.images
+      paragraphs = project.paragraphs.map do |paragraph|
+        {
+          project_id: project.id,
+          order_index: paragraph.order_index,
+          sentence_groups: paragraph.sentence_groups,
+          links: paragraph.links
+        }
+      end
       serialized_project = {
         title: title,
         paragraphs: paragraphs,
