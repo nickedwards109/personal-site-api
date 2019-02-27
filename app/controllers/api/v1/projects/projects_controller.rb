1e273327b2189127d161387c040f9b7160a97af1
nickedwards109 <nicholas.lee.edwards@gmail.com>
2019-02-27 18:57:47 -0500

    Implement Project alignment in the API response
    When a client requests the path 'api/v1/projects', the response includes a key-value pair indicating how the UI should align the project's text, as well as a key-value pair indicating how many content elements the UI should place on the left side of a center-split screen.

--- app/controllers/api/v1/projects/projects_controller.rb
+++ app/controllers/api/v1/projects/projects_controller.rb
@@ -3,6 +3,8 @@ class Api::V1::Projects::ProjectsController < ApplicationController
     projects = Project.all
 
     serialized_projects = projects.map do |project|
+      alignment = project.alignment
+      number_of_left_aligned_elements = project.number_of_left_aligned_elements
       title = project.title
       images = project.images
       paragraphs = project.paragraphs.map do |paragraph|
@@ -14,6 +16,8 @@ class Api::V1::Projects::ProjectsController < ApplicationController
         }
       end
       serialized_project = {
+        alignment: alignment,
+        number_of_left_aligned_elements: number_of_left_aligned_elements,
         title: title,
         paragraphs: paragraphs,
         images: images
