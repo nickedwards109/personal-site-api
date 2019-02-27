cb33c888d2811e577eb1079c583a3fc9512f1b76
nickedwards109 <nicholas.lee.edwards@gmail.com>
2019-02-27 17:54:31 -0500

    Specify attributes for Project alignment
    A project should have an attribute indicating whether the UI will align the display of the project in the center of the page, or split down the center of the page with content on the left and right. Also, a project should have an attribute indicating how many elements of content to put on the left side of a split display (implying that the remainder of elements will be placed on the right side).
    
    This will enable more variety in the UI layout by enabling some projects to be shown with visual emphasis on the left or right side instead of just the center.

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,13 +2,28 @@ require 'rails_helper'
 
 RSpec.describe Project, type: :model do
   it "has a title, has several paragraphs, and has several images" do
-    project = Project.create(title: "This is the Project title")
+    project = Project.create(
+      title: "This is the Project title",
+      alignment: "center",
+      number_of_left_aligned_elements: 3
+    )
 
     paragraphs = create_list(:paragraph, 2, project: project)
     images = create_list(:image, 2, project: project)
 
     expect(project).to be_valid
+    expect(project.title).to eq("This is the Project title")
+    expect(project.alignment).to eq("center")
+    expect(project.number_of_left_aligned_elements).to eq(3)
     expect(project.paragraphs.count).to eq(2)
     expect(project.images.count).to eq(2)
+
+    split_project = Project.create(
+      title: "This is the Project title",
+      alignment: "split_center",
+      number_of_left_aligned_elements: 3
+    )
+    expect(split_project).to be_valid
+    expect(split_project.alignment).to eq("split_center")
   end
 end
